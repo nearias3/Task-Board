9bdcfd7acddaa5dddc0d5e49b30bb3285038b974
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-06-17 00:20:53 -0500

    created the task card using jquery alterations to css, finally got tasks to appear in To Do list

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,13 +13,25 @@ function generateTaskId() {
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-    const taskCard = $('taskCard');
+   const card = $(`
+    <div class="card task-card mb-3" data-id="${task.id}">
+      <div class="card-body">
+        <h5 class="card-title">${task.title}</h5>
+        <p class="card-text">${task.description}</p>
+        <p class="card-text"><small class="text-muted">Due: ${task.dueDate}</small></p>
+        <button class="btn btn-danger delete-task">Delete</button>
+      </div>
+    </div>
+  `);
 
-
+  return card;
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
+    const taskList = JSON.parse(localStorage.getItem("tasks"));
+
+
 
 }
 
@@ -27,24 +39,25 @@ function renderTaskList() {
 function handleAddTask(event){
     event.preventDefault();   
 
-    const title = $('#task-title').val();
-    const dueDate = $('#task-due-date').val();
-    const formattedDueDate = dayjs(dueDate).format('MMMM D, YYYY');
-    const description = $('#task-description').val();
+    const title = $("#task-title").val();
+    const dueDate = $("#task-due-date").val();
+    const formattedDueDate = dayjs(dueDate).format("MMMM D, YYYY");
+    const description = $("#task-description").val();
 
-     
     const newTask = {
-        nextId: generateTaskId(),
-        title: title,
-        dueDate: formattedDueDate,
-        description: description
-        };
+      id: generateTaskId(),
+      title: title,
+      dueDate: formattedDueDate,
+      description: description,
+    };
 
 
-        taskList.push(newTask);
-        localStorage.setItem("tasks", JSON.stringify(taskList));
 
-        createTaskCard(newTask);
+    taskList.push(newTask);
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+
+    const taskCard = createTaskCard(newTask);
+    $("#todo-cards").append(taskCard);
 
 }
 
