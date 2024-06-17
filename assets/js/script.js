0edc7dcd9c78631385df68f03bf1ea8503ae1a5a
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-06-17 04:02:13 -0500

    Still doesn't work, undid more changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,6 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
-    if (nextId === null) {
-        nextId = 1;
-        localStorage.setItem("nextId", JSON.stringify(nextId));
-    }
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
